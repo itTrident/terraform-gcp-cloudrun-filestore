a38688d706a484918cf5e1d0c0242dfecca2e0f5
Selvakumar <98340923+s3lva-kumar@users.noreply.github.com>
2023-10-03 16:29:46 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,20 +1,20 @@
 module api-enable {
-  source = "./modules/api-enable"
+  source = "../modules/api-enable"
   api    = ["file.googleapis.com", "run.googleapis.com", "vpcaccess.googleapis.com"]
   disable_on_destroy = false
 }
 
 module filestore {
   module_enabled = true
-  source = "./modules/filestore"
+  source = "../modules/filestore"
   name = "tt"
   file_shares = [{capacity_gb = 1048, name = "test", nfs_export_options = [{ ip_ranges = ["0.0.0.0/0"] }]} ]
   network = [{network = "default", network_mode = ["MODE_IPV4"], connect_mode = "DIRECT_PEERING"}]
 }
 
 module serverless-vpc-access-connector {
-  module_enabled = false
-  source = "./modules/serverless-vpc-access-connector"
+  module_enabled = true
+  source = "../modules/serverless-vpc-access-connector"
   name = "test"
   ip_cidr_range = "10.2.0.0/28"
   network_vpc = "default"
@@ -22,8 +22,8 @@ module serverless-vpc-access-connector {
 
 
 module "cloud-runV2" {
-  module_enabled = false
-  source                     = "./modules/cloud-runV2"
+  module_enabled = true
+  source                     = "../modules/cloud-runV2"
   name                       = "nginx"
   execution_environment      = "EXECUTION_ENVIRONMENT_GEN2"
   containers_temp            = [{ name = "nginx", image = "nginx:latest", env = [{ name = "test", value = "testing" }], value_source = [], ports = [{ container_port = 80 }], volume_mounts = [] }]
