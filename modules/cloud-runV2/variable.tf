5de33dde4e71f0a70707cf01118964f7e43d6abb
Selvakumar <98340923+s3lva-kumar@users.noreply.github.com>
2023-10-11 17:39:27 +0530

    Update variable.tf

--- modules/cloud-runV2/variable.tf
+++ modules/cloud-runV2/variable.tf
@@ -12,7 +12,7 @@ variable location {
 variable project_id {
   description = "(Optional) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
   type        = string
-  default     = null
+  default     = ""
 }
 
 variable "execution_environment" {
